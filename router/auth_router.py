a8c186cd0807f1838e25c2ce968622d091a2c94b
andrescuello7 <andrescuellotrabajo@gmail.com>
2024-05-23 10:57:51 -0300

    add file of services with dates of db

--- router/auth_router.py
+++ router/auth_router.py
@@ -3,17 +3,18 @@ import bcrypt
 
 from jwt import encode
 from fastapi import HTTPException
-from fastapi.params import Depends, Depends
-from fastapi import APIRouter, Depends, Header, HTTPException
+from fastapi.params import Depends
+from fastapi import APIRouter, Header, HTTPException, status
 from schemas.auth_schema import auth_schema
 from models.user_model import UserModel
 from db_config import session, get_db
 from utils.auth import auth_token
+from services import auth_service as controller
 
 router = APIRouter()
 
 @router.get('/api/auth/user')
-def get_auth(authorization: str = Header(...), db: session = Depends(get_db)):
+def get_auth(authorization: str = Header(...)):
     try:
         response = auth_token(authorization)
         return response
@@ -21,24 +22,11 @@ def get_auth(authorization: str = Header(...), db: session = Depends(get_db)):
         return {'error': f'user not authenticated, details: {str(e)}'}
 
 @router.post('/api/auth/user')
-def create_auth(req: auth_schema, db: session = Depends(get_db)):
+def create_auth(
+    req: auth_schema,
+    db: session = Depends(get_db)):
     try:
-        user = db.query(UserModel).filter_by(user_name=req.user_name).first()
-        if user:
-            if not bcrypt.checkpw(
-                req.password.encode('utf-8'), user.password.encode('utf-8')
-            ):
-                raise HTTPException(401, detail="passwords do not match")
-
-            token = encode({
-                'user': {
-                    'id': user.id,
-                    'role': user.role,
-                    'full_name': user.full_name,
-                    'user_name': user.user_name,
-                    'photo': user.photo
-                }
-            }, os.getenv('JWT_SECRET'), algorithm='HS256')
-        return {'x-auth-token': token}
+        response = controller.create_auth(db, req)
+        return response
     except Exception:
-        raise HTTPException(401, detail="user not authenticated}")
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="user not authenticated}")
--- router/user_router.py
+++ router/user_router.py
@@ -1,12 +1,11 @@
-from fastapi.params import Depends, Depends
-from fastapi import APIRouter, Depends, HTTPException, Header
+from fastapi import APIRouter, HTTPException, Header, status
+from fastapi.params import Depends
 from starlette.responses import RedirectResponse
 from enums.user_enum import user_role
-from models.user_model import UserModel
 from schemas.user_schema import user_schema
 from db_config import session, get_db
 from utils.auth import auth_token
-from utils.bcrypt import hash_password
+from services import user_service as controller
 
 router = APIRouter()
 
@@ -16,8 +15,11 @@ def main():
 
 @router.get('/api/users/getAll')
 def get_users(db: session = Depends(get_db)):
-    try: 
-        response = db.query(UserModel).all()
+    '''
+    Get all Users
+    '''
+    try:
+        response = controller.get_all(db)
         return response
     except Exception as e:
         return {'error': f"get all users, detail: {e}"}
@@ -27,19 +29,15 @@ def create_user(
     req: user_schema,
     authorization: str = Header(...),
     db: session = Depends(get_db)):
+    '''
+    Create new User
+    '''
     try:
-        user_auth = auth_token(authorization)
-        if user_auth['role'] != user_role().ADMIN:
-            raise HTTPException(400, detail='Not authorized')
-        response = UserModel(
-            photo=req.photo,
-            user_name=req.user_name,
-            full_name=req.full_name,
-            password=hash_password(req.password),
-        )
-        db.add(response)
-        db.commit()
-        db.refresh(response)
+        if auth_token(authorization)['role'] != user_role().ADMIN:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                detail='Not authorized')
+
+        response = controller.create_new_user(db, req)
         return response
     except Exception as e:
         return {'error': f"create user, detail: {e}"}
@@ -50,37 +48,33 @@ def update_task(
     user_id: int,
     authorization: str = Header(...),
     db: session = Depends(get_db)):
+    '''
+    Update a task of admin
+    '''
     try:
-        user_auth = auth_token(authorization)
-        if user_auth['role'] != user_role().ADMIN:
-            raise HTTPException(400, detail='Not authorized')
-        user = db.query(UserModel).filter_by(id=user_id).first()
-        if user:
-            user.photo = req.photo
-            user.user_name = req.user_name
-            user.full_name = req.full_name
-            user.password = hash_password(req.password)
-            user.role = req.role
+        if auth_token(authorization)['role'] != user_role().ADMIN:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                detail='Not authorized')
 
-            db.commit()
-            db.refresh(user)
-            return user
+        response = controller.update_user(db, user_id, req)
+        return response
     except Exception as e:
-        return {'error': "update user"}
+        return {'error': f"update user, {str(e)}"}
 
 @router.delete('/api/delete/user/{user_id}')
 def delete_character(
     user_id: int,
     authorization: str = Header(...),
     db: session = Depends(get_db)):
+    '''
+    Delete a user
+    '''
     try:
-        user_auth = auth_token(authorization)
-        if user_auth['role'] != user_role().ADMIN:
-            raise HTTPException(400, detail='Not authorized')
-        user = db.query(UserModel).filter_by(id=user_id).first()
-        if user:
-            db.delete(user)
-            db.commit()
-            return {"message": "User deleted successfully", "user": user}
+        if auth_token(authorization)['role'] != user_role().ADMIN:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                detail='Not authorized')
+
+        user = controller.delete_user(db, user_id)
+        return {"message": "User deleted successfully", "user": user}
     except Exception as e:
-        return {'error': "delete user"}
+        return {'error': f"delete user, {str(e)}"}
